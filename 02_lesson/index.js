cede17f7f0ad3df66e538466a7b89726202fbda2
DebasishSadangi2002 <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-09-11 09:06:40 +0530

    random ID genrator

--- 02_lesson/index.js
+++ 02_lesson/index.js
@@ -2,6 +2,7 @@ console.log("Hello Node JS");
 
 
 const { format } = require('date-fns');
-
+const { v4: uuid} = require('uuid')
 
 console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+console.log("Generted Random id :", uuid());
